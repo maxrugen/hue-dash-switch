b7c5820b357ce65d4ee6d1e3c8de82027ed7243b
Hywel Rees <hjr555@gmail.com>
2017-10-08 16:00:33 +0100

    Fixed bug in registerUser

--- registerUser.js
+++ registerUser.js
@@ -1,6 +1,6 @@
 require('dotenv').load();
 const hueAPI = require("node-hue-api").HueApi;
-const hueIP = process.env.HUEIP,
+const hueIP = process.env.HUEIP;
 const newUserName = null;  // The Hue Bridge will generate a User ID
 const userDescription = "Amazon Dash Button User";
 
